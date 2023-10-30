c9f2044005c63d4580243ca197bcf539dc9317ec
NditiE <ellamarlene54@gmail.com>
2023-10-30 22:50:29 +0300

    done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function Item({ name, category }) {
+  const itemRef = useRef(null);
+
+  const handleAddToCart = () => {
+    if (itemRef.current) {
+      itemRef.current.classList.add("in-cart");
+    }
+  };
+
   return (
-    <li className="">
+    <li ref={itemRef} className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToCart}>
+        Add to Cart
+      </button>
     </li>
   );
 }
